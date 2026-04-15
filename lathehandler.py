4785d37ffaff59013074ec51ddb8a7cd7cb0f2e4
Ellyan <ellyan@electrolab.fr>
2026-04-15 13:11:12 +0200

    Adding a nodro switch to make the DRO optionnal.

--- lathehandler.py
+++ lathehandler.py
@@ -41,6 +41,7 @@ from gi.repository import GdkPixbuf
 debug = 0
 notouch = 0
 norun = 0
+nodro = 0
 svgfile = os.path.join(os.path.dirname(__file__), "LatheMacro.svg")
 
 class HandlerClass:
@@ -123,6 +124,13 @@ class HandlerClass:
         if norun:
             for c in range(0,7):
                 self.builder.get_object(f'tab{c}.action').set_visible(False)
+
+        if nodro:
+            for c in range(0, 7):
+                for axis in ('X', 'Z'):
+                    w = self.builder.get_object(f'tab{c}.dro{axis}')
+                    if w:
+                        w.set_visible(False)
 
     def show_keyb(self, obj, data=None):
         if notouch: return False
